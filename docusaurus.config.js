11a77900a843a9551ecc10d82c47818d6e2a7c51
Iam-PAIN <mrpainehere@gmail.com>
2022-11-09 11:45:19 +0530

    plusblog: Fixed typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favi.ico',
+  favicon: 'img/Favi.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
